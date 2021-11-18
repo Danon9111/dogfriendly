1f6012fbe6533b2174dc841ddc72661510768b00
Andrzej Dolinski <andrzej.dolinski@PL-MAC-C02C803XLVDM.local>
2021-11-18 20:59:33 +0100

    adding callMap function to display map on the main page

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
-var mapsApiKey = config.MAPS_API_KEY;
-
-let map;
+const mapsApiKey = config.MAPS_API_KEY;
 
 function initMap() {
+  let map;
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 52.409538, lng: 16.931992 },
     zoom: 10,
   });
+}
+
+function callMap(url) {
+  let script = document.createElement('script');
+  script.src = url;
+  script.async = true;
+  document.body.appendChild(script);
 }
