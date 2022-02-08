5fe3de2ec037a54476eb3d88ed44b17e30c4a5aa
Andrzej Dolinski <andrzej.dolinski@PL-MAC-C02C803XLVDM.local>
2022-02-08 21:11:33 +0100

    Set up Places/Places Searches API with default capabilities

--- js/index.js
+++ js/index.js
@@ -1,16 +1,47 @@
 const mapsApiKey = config.MAPS_API_KEY;
-
-function initMap() {
-  let map;
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 52.409538, lng: 16.931992 },
-    zoom: 10,
-  });
-}
+let map;
 
 function callMap(url) {
   let script = document.createElement('script');
   script.src = url;
   script.async = true;
   document.body.appendChild(script);
+}
+
+function initAutocomplete() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 52.409538, lng: 16.931992 },
+    zoom: 15,
+    mapTypeId: "roadmap",
+  });
+
+  const request = {
+    query: "Stary Rynek",
+    fields: ["name", "geometry"],
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.findPlaceFromQuery(request, (results, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+      for (let i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+
+      map.setCenter(results[0].geometry.location);
+    }
+  });
+}
+
+function createMarker(place) {
+  if (!place.geometry || !place.geometry.location) return;
+
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent(place.name || "");
+    infowindow.open(map);
+  });
 }
