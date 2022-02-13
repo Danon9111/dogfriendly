01a3830be04cda4d453aeb740fb1b90bccd34607
Andrzej Dolinski <andrzej.dolinski@PL-MAC-C02C803XLVDM.local>
2022-02-13 21:30:11 +0100

    displaying markers from provided json

--- js/index.js
+++ js/index.js
@@ -22,13 +22,32 @@ function initMap() {
   new google.maps.Marker({
     position: cityCenterCoordinates,
     map,
-    title: "Hello World!",
   });
+
+  const script = document.createElement("script");
+
+  script.src =
+    "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
+  document.getElementsByTagName("head")[0].appendChild(script);
+
+  
   
   //hidding default points on map
   map.setOptions({ styles: styles["hide"] });
 
 }
+
+const eqfeed_callback = function (results) {
+  for (let i = 0; i < results.features.length; i++) {
+    const coords = results.features[i].geometry.coordinates;
+    const latLng = new google.maps.LatLng(coords[1], coords[0]);
+
+    new google.maps.Marker({
+      position: latLng,
+      map,
+    });
+  }
+};
 
 const styles = {
   hide: [
