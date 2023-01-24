f0a33f27cd8a909a412db74704088d28b895cd12
andrzej d <4902552+Danon9111@users.noreply.github.com>
2023-01-24 20:18:13 +0100

    fixing formatting and fixing displaying location details in pop-up

--- js/maps.js
+++ js/maps.js
@@ -3,26 +3,33 @@ const poznanCenter = { lat: 52.409538, lng: 16.931992 };
 let marker, i, map;
 
 function displayMarkers() {
-  fetch('./assets/locations.json') //read file from a location
+  fetch("./assets/locations.json") //read file from a location
     .then(function (response) {
       return response.json(); //to get JSON data from response need to be executed json() function. It returns promise
     })
-    .then(function (places) { //get JSON data as a parameter
-      for (i = 0; i < places?.locations.length; i++) { 
+    .then(function (places) {
+      //get JSON data as a parameter
+      let infowindow = new google.maps.InfoWindow();
+      for (i = 0; i < places?.locations.length; i++) {
         marker = new google.maps.Marker({
-          position: new google.maps.LatLng(places?.locations[i]["lat"], places?.locations[i]["lng"]),
-          map: map
-        }); 
+          position: new google.maps.LatLng(
+            places?.locations[i]["lat"],
+            places?.locations[i]["lng"]
+          ),
+          map: map,
+        });
+
+        google.maps.event.addListener(
+          marker,
+          "click",
+          (function (marker, i) {
+            return function () {
+              infowindow.setContent(places.locations[i]["name"]);
+              infowindow.open(map, marker);
+            };
+          })(marker, i)
+        );
       }
-
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          console.log(places?.locations[i][0]);
-          infowindow.setContent(places.locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-
     })
     .catch(function (err) {
       console.log(err);
@@ -30,8 +37,6 @@ function displayMarkers() {
 }
 
 function initMap() {
-  let infowindow = new google.maps.InfoWindow();
-
   map = new google.maps.Map(document.getElementById("map"), {
     center: poznanCenter,
     zoom: 13,
@@ -40,9 +45,8 @@ function initMap() {
   displayMarkers();
 }
 
-
 function callMap(url) {
-  let script = document.createElement('script');
+  let script = document.createElement("script");
   script.src = url;
   script.async = true;
   document.body.appendChild(script);
